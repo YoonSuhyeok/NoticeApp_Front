95bbaed928d326052569ca7e2ed4eaad0dd5d992
YoonSuhyeok <cooltime15@likelion.org>
2022-08-22 10:49:47 +0900

    Fix eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,55 @@
 module.exports = {
-  root: true,
-  extends: [
-    '@react-native-community',
-    'airbnb',
-    'airbnb-typescript'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
+    root: true,
+    extends: [
+        '@react-native-community',
+        'airbnb',
+        'airbnb/hooks',
+        'airbnb-typescript',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: './tsconfig.json',
     },
-  ],
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off',
+            },
+        },
+    ],
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        'react/function-component-definition': [
+            2,
+            { namedcomponents: 'arrow-function' },
+        ],
+        "@typescript-eslint/indent": "off",
+        "react/jsx-indent": "off",
+        "react/jsx-indent-props": "off",
+        "react/jsx-closing-bracket-location": "off",
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+              "js": "never",
+              "jsx": "never",
+              "ts": "never",
+              "tsx": "never"
+            }
+        ],
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto'
+            },
+        ],
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off'
+    },
 };
