25e62ac765fd9e229e78884bf08301695d96e818
YoonSuhyeok <cooltime15@likelion.org>
2022-08-26 10:08:34 +0900

    Add NoticeBox Component

--- components/noticeBox.tsx
+++ components/noticeBox.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from 'twrnc';
 
-function NoticeBox() {
-    return <View>Init</View>;
+interface Notice {
+    title: string;
+}
+
+function NoticeBox({ title }: Notice) {
+    return (
+        <View style={tw`p-2 mt-2 border-solid border-2 border-slate-400`}>
+            <Text style={tw`text-lg font-bold`}>{title}</Text>
+            <View style={tw`flex flex-row`}>
+                <Text>2022.08.25</Text>
+                <Text> || </Text>
+                <Text>예비군연대</Text>
+            </View>
+        </View>
+    );
 }
 
 export default NoticeBox;
--- page/homeScreen.tsx
+++ page/homeScreen.tsx
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 
+import NoticeBox from '../components/noticeBox';
 import SearchBar from '../components/searchBar';
+
+const data = [
+    {
+        id: 1,
+        title: '첫번째 공지',
+    },
+    {
+        id: 2,
+        title: '첫번째 공지',
+    },
+    {
+        id: 3,
+        title: '첫번째 공지',
+    },
+    {
+        id: 4,
+        title: '첫번째 공지',
+    },
+    {
+        id: 5,
+        title: '첫번째 공지',
+    },
+    {
+        id: 6,
+        title: '첫번째 공지',
+    },
+];
 
 function HomeScreen() {
     const [searchWord, setSearchWord] = useState<string>('');
@@ -11,6 +39,10 @@ function HomeScreen() {
                 styles="mt-5"
                 word={searchWord}
                 setWord={setSearchWord}
+            />
+            <FlatList
+                data={data}
+                renderItem={item => <NoticeBox title={item.item.title} />}
             />
         </View>
     );
