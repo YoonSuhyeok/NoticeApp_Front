ef3531de1ffc03a2d0a76084491826b812a5f26e
YoonSuhyeok <cooltime15@likelion.org>
2022-09-02 12:20:50 +0900

    Add Tag & Css in Select

--- page/SelectScreen.tsx
+++ page/SelectScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import tw from 'twrnc';
 
 import useReadColleges from '../api/college/useReadColleges';
-import Box from '../components/box';
 
 function SelectScreen() {
     const { data, isLoading } = useReadColleges();
@@ -13,14 +14,59 @@ function SelectScreen() {
 
     return (
         <View>
-            {data &&
-                data.map(college => (
-                    <Box
-                        key={college.id.toString()}
-                        name={college.name}
-                        majors={college.majors}
-                    />
-                ))}
+            <View style={tw`flex flex-row items-center h-10 bg-sky-400`}>
+                <Ionicons
+                    style={tw`ml-2`}
+                    name="arrow-back"
+                    size={16}
+                    color="white"
+                />
+                <Text style={tw`text-white ml-2`}>검색필터</Text>
+            </View>
+            <View style={tw`flex flex-row justify-between`}>
+                <View style={tw`w-1/2 border-r-2 border-slate-400`}>
+                    <View
+                        style={tw`flex flex-row items-center border-b-2 border-slate-400 h-11`}>
+                        <View style={tw`w-2.5 h-10.5 bg-orange-300`} />
+                        <Text style={tw`font-bold text-base ml-2`}>
+                            학과선택
+                        </Text>
+                    </View>
+                    <View style={tw`h-full`}>
+                        {data &&
+                            data.map(college => (
+                                <View
+                                    style={tw`flex flex-row items-center border-b-2 border-slate-400 h-11`}>
+                                    <Text
+                                        key={college.id}
+                                        style={tw`text-sm ml-8 flex-4`}>
+                                        {college.name}
+                                    </Text>
+                                    <Ionicons
+                                        style={tw`flex-1`}
+                                        name="checkmark"
+                                        size={16}
+                                    />
+                                </View>
+                            ))}
+                    </View>
+                </View>
+                <View style={tw`w-1/2`}>
+                    {data &&
+                        data.map(college =>
+                            college.majors.map(major => (
+                                <View
+                                    style={tw`flex flex-row items-center h-11 w-full`}>
+                                    <Text
+                                        key={major.name}
+                                        style={tw`text-sm pl-8`}>
+                                        · {major.name}
+                                    </Text>
+                                </View>
+                            )),
+                        )}
+                </View>
+            </View>
         </View>
     );
 }
